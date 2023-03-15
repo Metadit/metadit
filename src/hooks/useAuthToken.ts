1fc4f73042067034e167baebe666162a5864e3c6
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2023-03-15 13:09:08 +0300

    tweaks

--- src/hooks/useAuthToken.ts
+++ src/hooks/useAuthToken.ts
@@ -3,9 +3,12 @@ import jwt_decode from "jwt-decode";
 import { refreshTokensService } from "../services/authentication";
 import { IUserLocalStorage } from "../types/user";
 import { USER_TOKEN_KEY } from "../constants";
+import { toast } from "react-hot-toast/headless";
+import { useUser } from "../contexts/User";
 
 export const useAuthToken = (): string => {
     const REFRESH_INTERVAL = 60 * 1000;
+    const { setUser } = useUser();
     const [jwt, setJwt] = useState<string>("");
     const userData =
         typeof window !== "undefined" &&
@@ -15,14 +18,16 @@ export const useAuthToken = (): string => {
         if (!userData?.token) return;
         setJwt(userData.token);
         const interval = setInterval(() => {
-            refreshTokenIfExpiringSoon(userData, jwt, setJwt).catch(error => {
-                console.log(error);
+            refreshTokenIfExpiringSoon(userData, jwt, setJwt).catch(() => {
+                toast.error("Error refreshing token");
+                setUser(null);
+                localStorage.removeItem(USER_TOKEN_KEY);
             });
         }, REFRESH_INTERVAL);
         return () => {
             clearInterval(interval);
         };
-    }, [REFRESH_INTERVAL, jwt, userData]);
+    }, [REFRESH_INTERVAL, jwt, setUser, userData]);
 
     return jwt;
 };
@@ -31,7 +36,7 @@ async function refreshTokenIfExpiringSoon(
     userData: IUserLocalStorage,
     jwt: string,
     setJwt: Dispatch<SetStateAction<string>>
-) {
+): Promise<void> {
     const decodedToken: {
         exp: number;
         iat: number;
